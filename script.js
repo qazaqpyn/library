63454e3c9e30162f179480c6e1d7b9cdd7e00dff
Ali <alimkali.alizhan@gmail.com>
2020-10-29 01:48:23 +0600

    web Storage API; localStorage

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
-let myLibrary=[];
+let conditionWebStorage;
+let myLibrary = [];
+let storageLib; 
 const table= document.querySelector("tbody");
 const form = document.querySelector("form");
 const btnNew = document.querySelector(".newBook");
 const btnAdd = document.querySelector(".addBook")
 let na = false;
+
+// Feature-detecting localStorage
+//testing for availablity 
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        let x = '__storage_test__';
+        storage.setItem(x,x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            //everything except Firefox
+            e.code === 22 ||
+            //Firefox
+            e.code === 1014 ||
+            //test name field(code might not be present)
+            // everything except Firefox
+            e.name==='QuotaExceededError' ||
+            // Firefox
+            e.name==='NS_ERROR_DOM_QUOTA_REACHED') && 
+            (storage&&storage.length !==0);
+    }
+}
+
+
+if (storageAvailable('localStorage')) {
+    conditionWebStorage = true;
+} else {
+    conditionWebStorage = false;
+} 
+
+if (conditionWebStorage) {
+    if (localStorage.length === 0) {
+        populateStorage();
+    } else {
+        setStyles();
+    }
+} else {
+    storageLib = myLibrary;
+}
+
+function populateStorage() {
+    localStorage.setItem('library',JSON.stringify(myLibrary));
+    setStyles();
+}
+
+function setStyles(){
+    storageLib = localStorage.getItem('library');
+    console.log(storageLib);
+    myLibrary = JSON.parse(storageLib);
+    console.log(myLibrary);
+}
 
 function Book(title, author, pages, read) {
     this.title=title;
@@ -15,16 +72,24 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title,author,pages,read) {
     const mybook= new Book(title, author,pages,read);
     myLibrary.push(mybook);
+    if (title !== "N/A" && read !== "N/A"){
+        localStorage.setItem('library',JSON.stringify(myLibrary));
+    } 
+   
+    console.log(localStorage.getItem('library'));
     refresh();
 }
 
-Book.prototype.toggleRead = function() {
-    if (this.read) {
-        this.read=false;
-    } else {
-        this.read=true;
-    }
-}
+// there's a problem with constructor prototype when working JSON parsed objects
+
+// Book.prototype.toggleRead = function() {
+//     console.log(this);
+//     if (this.read) {
+//         this.read=false;
+//     } else {
+//         this.read=true;
+//     }
+// }
 
 //TODO: for looping over
 function refresh() {
@@ -61,16 +126,27 @@ function refresh() {
     
             deleteBtn.addEventListener('click',(e) => {
                 myLibrary.splice(e.target.dataset.rowNumber,1)
+                localStorage.setItem('library',JSON.stringify(myLibrary));
                 refresh();
             })
             changeBtn.addEventListener('click',(e) => {
-                myLibrary[e.target.dataset.rowNumber].toggleRead();
+                console.log(myLibrary[e.target.dataset.rowNumber]);
+                // Object.assign(new Book,myLibrary[e.target.dataset.rowNumber]);
+                // myLibrary[e.target.dataset.rowNumber].toggleRead();
+                if (myLibrary[e.target.dataset.rowNumber].read) {
+                    myLibrary[e.target.dataset.rowNumber].read=false;
+                } else {
+                    myLibrary[e.target.dataset.rowNumber].read=true;
+                }
+                localStorage.setItem('library',JSON.stringify(myLibrary));
+                // console.log(myLibrary[e.target.dataset.rowNumber]);
                 refresh();
             })
         }
         // making N/A dissappear when adding new book
         if (na && myLibrary.length > 0) {
             myLibrary.splice(0,1);
+            localStorage.setItem('library',JSON.stringify(myLibrary));
             na = false;
             refresh();
         }
@@ -116,5 +192,9 @@ btnNew.addEventListener("click", () => {
     }
 }) 
 
+
+
+
 refresh();
-// addBookToLibrary("how","me",123,true);
+
+
